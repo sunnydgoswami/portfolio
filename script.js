27c074149bc7bec0063ac4cce7aff7be470664ab
sunnydgoswami <sunnydgoswami@gmail.com>
2017-06-09 20:22:35 -0400

    I had to update/change the nav bar...ended up realizing I have to inclue the attributes on the entire body for scroll to work. I added js for the scroll i added some css and I changed nav in my html...

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// $(document).ready(function() {
+//   var waypoint = new Waypoint({
+//     element: document.getElementById('aboutSec'),
+//     handler: function(direction) {
+//       alert('I am 20px from the top ' + 'Direction: ' + direction)
+//     },
+//     offset:20
+//   })
+// });
+
+
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+
+
 // When the DOM is ready, run this function
 $(document).ready(function() {
   //Set the carousel options
